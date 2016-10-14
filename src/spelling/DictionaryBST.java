a0e4c9f77f5898b8a3a3dbe5aeee9f839560340b
Jaya Kasa <kasa288@gmail.com>
2016-10-14 14:03:01 -0400

    implemented DictionaryBST and DictionaryLL
    DictionaryBenchmarking should be optimized to use threads

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -12,6 +12,9 @@ public class DictionaryBST implements Dictionary
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
+    public DictionaryBST(){
+        dict = new TreeSet<>();
+    }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -19,9 +22,11 @@ public class DictionaryBST implements Dictionary
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
+    //O(lgn)
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+        word = word.toLowerCase();
+        return dict.add(word);
     }
 
 
@@ -29,13 +34,14 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
+    //O(lgn)
     public boolean isWord(String s) {
     	//TODO: Implement this method
-        return false;
+        return dict.contains(s.toLowerCase());
     }
 
 }
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -6,12 +6,13 @@ import java.util.LinkedList;
  * A class that implements the Dictionary interface using a LinkedList
  *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
 	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
+
+    public DictionaryLL(){
+        dict = new LinkedList<>();
+    }
 
 
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -19,23 +20,35 @@ public class DictionaryLL implements Dictionary
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
+    //O(1)
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        word = word.toLowerCase();
+        if(dict.contains(word))
+            return false;
+        return dict.add(word);
     }
 
 
     /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
-        return 0;
+    public int size() {
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
+    //O(n)
     public boolean isWord(String s) {
         //TODO: Implement this method
-        return false;
+        /*
+        int cap = 0;
+
+        for(int a = 0; a < s.length(); a++){
+            if(Character.isUpperCase(s.charAt(a)))
+                cap++;
+        }
+        if(cap > 1 || cap == 0)
+            return false;
+        */
+        return dict.contains(s.toLowerCase());
     }
 
     
